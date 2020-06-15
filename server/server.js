42bffc4d1d05fd6416c571a64f9ed3ce86609f0f
Juan Gomez <juang77@hotmail.com>
2020-06-15 16:40:08 -0500

    Nmp start listo

--- server/server.js
+++ server/server.js
@@ -41,6 +41,6 @@ app.delete('/usuario', function(req, res) {
     res.json('delete Usuario')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto : ', process.env.PORT);
 });
